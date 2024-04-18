be9618917121a00e29bca8ad3116de32a5aaff6b
Guilherme Costa <101594185+guihocosta@users.noreply.github.com>
2024-04-18 12:52:50 -0300

    Add files via upload

---
+++ exercicio 7/js/script.js
@@ -0,0 +1,55 @@
+let i = 0;
+
+function menu() {
+    const menu = document.getElementsByClassName("menu");
+
+    if (i === 0) {
+        menu[0].style.display = "inline";
+        i = i + 1;
+
+    }
+
+    else {
+        menu[0].style.display = "none";
+        i = i - 1;
+    }
+}
+
+function autenticar() {
+    const usuario = document.getElementById("idUsu");
+    const txtUsu = usuario.value;
+
+    const password = document.getElementById("idPwd");
+    const txtPwd = password.value;
+
+    const p1 = document.getElementById("idAviso");
+    const p2 = document.getElementById("idAviso2");
+
+
+    if (txtUsu == "") {
+        usuario.style.borderBlockColor = "red";
+        p1.innerText = "Usuário precisa ser preenchido";
+    }
+
+    else {
+        usuario.style.borderBlockColor = "";
+        p1.innerText = "";
+    }
+
+    if (txtPwd == "") {
+        password.style.borderBlockColor = "red";
+        p2.innerText = "Senha precisa ser preenchida";
+    }
+
+    else if (txtPwd.length <= 6 || txtPwd.length >= 30) {
+        console.log("Salve");
+        password.style.borderBlockColor = "red";
+        p2.innerText = "Senha deve ter entre 6 a 30 caracteres";
+    }
+
+
+    else {
+        password.style.borderBlockColor = "";
+        p2.innerText = "";
+    }
+}
